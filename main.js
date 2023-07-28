70c2440d41598077ab4579c151c69ca495ab1f8e
nicolas moyano <moyano.nico91@gmail.com>
2023-07-28 14:28:30 -0300

    modificacion

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let marca, modelo, descuento=0;
+let marca="", modelo="", descuento=0;
 
-marca= prompt("marca?","0");
-modelo= prompt("modelo?","0");
+marca = prompt("Marca?");
+modelo = prompt("Modelo?");
 
-if (marca== "chevrolet")
-    if (modelo == "onix")
+if (marca === "chevrolet")
+    if (modelo === "onix")
     {
         descuento=5;
     }
@@ -13,34 +13,37 @@ if (marca== "chevrolet")
     }
     alert (`Su descuento es ${descuento}`);
 
+    for(let turno=1; turno <= 10; turno ++)
+    {
+        let nombre = prompt("Ingrese su Nombre");
+        alert (`Turno #${turno} Nombre: ${nombre} `)
+    }
 
-//Funcion para ingresar cantidad a prestar
-function cantidad1(){
-    let cantidad1 = parseInt(prompt("ingresar cantidad a prestar"));
+    function cantidad1(){
+    let cantidad1 = prompt("ingresar cantidad a prestar");
     alert("La cantidad es = " + cantidad1)
-    return cantidad1
+    return cantidad1;
     }
     
-    //Funcion para ingresar cuotas a pagar
     function cuotas1(){
-    let cuotas1 = parseInt(prompt("En cuantas cuotas quiere pagar, elija de 1 a 12"));
+    let cuotas1 =prompt("En cuantas cuotas quiere pagar, elija de 1 a 12");
     alert("Desea el prestamo en " + cuotas1 + " cuotas?")
     return cuotas1
     }
     
-    let cantidad = cantidad1()
-    let cuotas = cuotas1()
+    let cantidad = cantidad1();
+    let cuotas = cuotas1();
     
-    console.log("La cantidad es: " + cantidad)
-    console.log("Las cuotas son:" + cuotas)
+    console.log("La cantidad es: " + cantidad);
+    console.log("Las cuotas son:" + cuotas);
     
     function dividir(dato1, dato2){
     let resultado = dato1 / dato2;
-    return resultado
+    return resultado;
     }
     
     //Capturar cantiddad / cuotas
-    let division = dividir(cantidad, cuotas)
+    let division = dividir(cantidad, cuotas);
     
     //Mostrar resultado
-    console.log("Usted debe pagar " + cuotas + " cuotas de " + division)
+    console.log("Usted debe pagar " + cuotas + " cuotas de " + division);
---
+++ prueba.js
@@ -0,0 +1,56 @@
+let numero= parseInt(prompt("ingrese un numero"));
+for(let i=1;i<10;i++){
+alert(numero *i);
+}
+
+
+let entrada = prompt("Ingresar un nombre");
+
+while (entrada != "ESC") {
+    switch (entrada) {
+        case "ANA":
+            alert("Hola ANA");
+            break;
+
+    case "JUAN":
+        alert("Hola JUAN");
+        break;
+
+    default:
+        alert("Quién eres?");
+        break;
+}
+    entrada = prompt("Ingresar un nombre");
+}
+
+
+ //Funcion para ingresar cantidad a prestar
+    function cantidad1(){
+    let cantidad1 = parseInt(prompt("ingresar cantidad a prestar"));
+    alert("La cantidad es = " + cantidad1)
+    return cantidad1
+    }
+    
+    //Funcion para ingresar cuotas a pagar
+    function cuotas1(){
+    let cuotas1 = parseInt(prompt("En cuantas cuotas quiere pagar, elija de 1 a 12"));
+    alert("Desea el prestamo en " + cuotas1 + " cuotas?")
+    return cuotas1
+    }
+    
+    let cantidad = cantidad1()
+    let cuotas = cuotas1()
+    
+    console.log("La cantidad es: " + cantidad)
+    console.log("Las cuotas son:" + cuotas)
+    
+    function dividir(dato1, dato2){
+    let resultado = dato1 / dato2;
+    return resultado
+    }
+    
+    //Capturar cantiddad / cuotas
+    let division = dividir(cantidad, cuotas)
+    
+    //Mostrar resultado
+    console.log("Usted debe pagar " + cuotas + " cuotas de " + division)

575d369cc101614b795b2736a31414477255392a
nicolas moyano <moyano.nico91@gmail.com>
2023-09-26 11:27:31 -0300

    fetch y local storage

--- main.js
+++ main.js
@@ -1,129 +1,165 @@
-let products=[];
 fetch("./products.json")
-    .then(response => response.json())
-    .then(data => {
-    productos = data;
-    })
+  .then((response) => response.json())
+  .then((data) => {
+    mostrarProductos (data);
+  });
 
+const containeritems = document.getElementById((id = "containeritems"));
+const iconcart = document.getElementById((id = "iconcart"));
+const modalcontainer = document.getElementById((id = "modalcontainer"));
 
-const containeritems = document.getElementById(id = "containeritems");
-const iconcart = document.getElementById(id="iconcart");
-const modalcontainer= document.getElementById(id="modalcontainer");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let carrito = [];
-
-const productos = [
-    { id: 1, nombre: "Chevrolet Onix", precio: 5270000, img:"./assets/onix.jpg",cantidad:1},
-    { id: 2, nombre: "Chevrolet Prisma", precio: 5400000,img:"./assets/prisma.jpg",cantidad:1},
-    { id: 3, nombre: "Chevrolet Cruze", precio: 7690000,img:"./assets/cruze.jpg", cantidad:1},
-    { id: 4, nombre: "Ford Fiesta", precio: 5770000,img:"./assets/fiesta.jpg",cantidad:1},
-    { id: 5, nombre: "Ford Mondeo", precio: 12350000,img:"./assets/mondeo.jpg", cantidad:1},
-    { id: 6, nombre: "Ford Ranger", precio: 15530000, img:"./assets/ranger.jpg", cantidad:1},
-];
-
-productos.forEach((item) => { 
+function mostrarProductos(products) {
+  products.forEach((item) => {
     let div = document.createElement("div");
     div.className = "card";
     div.innerHTML = `
-    <img class "img" src="${item.img}"/> 
-    <p class"info-product" >${item.nombre}</p>
-    <b class "price">${item.precio} $</b>
-`;
-containeritems.append (div);
+      <img class "img" src="${item.img}"/> 
+      <p class"info-product" >${item.nombre}</p>
+      <b class "price">${item.precio} $</b>
+  `;
+    containeritems.append(div);
 
-let agregar = document.createElement ("button");
-agregar.innerText= "Agregar";
-agregar.classname= "button";
+    let agregar = document.createElement("button");
+    agregar.innerText = "Agregar";
+    agregar.classname = "button";
 
-div.append(agregar);
+    div.append(agregar);
 
-agregar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatProduct)=> repeatProduct.id === item.id);
-    if(repeat) {
-        carrito.map((prod)=>{
-            if(prod.id === productos.id){
+    agregar.addEventListener("click", () => {
+      const repeat = carrito.some(
+        (repeatProduct) => repeatProduct.id === item.id
+      );
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === productos.id) {
             prod.cantidad++;
-                }
+          }
         });
-        } else {
-            carrito.push({
-                id:item.id,
-                img:item.img,
-                nombre: item.nombre,
-                precio: item.precio,
-                cantidad: item.cantidad,
-            });
-            console.log(carrito);
-            savelocal();
-            }
-            Swal.fire({
-                title: 'Queres agregar el producto?',
-                showDenyButton: true,
-                showCancelButton: true,
-                confirmButtonText: 'Save',
-                denyButtonText: `Don't save`,
-            }).then((result) => {
-                /* Read more about isConfirmed, isDenied below */
-                if (result.isConfirmed) {
-                Swal.fire('Agregado!', '', 'success')
-                } else if (result.isDenied) {
-                Swal.fire('No Agredado', '', 'info')
-                }
-            })
+      } else {
+        carrito.push({
+          id: item.id,
+          img: item.img,
+          nombre: item.nombre,
+          precio: item.precio,
+          cantidad: item.cantidad,
         });
-    });   
-    const vercarrito =()=>{
-    modalcontainer.innerHTML= "";  
-    modalcontainer.style.display= "flex";
-    const modalHeader =document.createElement("div");
-    modalHeader.className= "modal-header";
-    modalHeader.innerHTML= `
+        console.log(carrito);
+        savelocal();
+      }
+      Swal.fire({
+        title: "Queres agregar el producto?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Agregado!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("No Agredado", "", "info");
+        }
+      });
+    });
+  });
+}
+
+// const productos = [
+//   {
+//     id: 1,
+//     nombre: "Chevrolet Onix",
+//     precio: 5270000,
+//     img: "./assets/onix.jpg",
+//     cantidad: 1,
+//   },
+//   {
+//     id: 2,
+//     nombre: "Chevrolet Prisma",
+//     precio: 5400000,
+//     img: "./assets/prisma.jpg",
+//     cantidad: 1,
+//   },
+//   {
+//     id: 3,
+//     nombre: "Chevrolet Cruze",
+//     precio: 7690000,
+//     img: "./assets/cruze.jpg",
+//     cantidad: 1,
+//   },
+//   {
+//     id: 4,
+//     nombre: "Ford Fiesta",
+//     precio: 5770000,
+//     img: "./assets/fiesta.jpg",
+//     cantidad: 1,
+//   },
+//   {
+//     id: 5,
+//     nombre: "Ford Mondeo",
+//     precio: 12350000,
+//     img: "./assets/mondeo.jpg",
+//     cantidad: 1,
+//   },
+//   {
+//     id: 6,
+//     nombre: "Ford Ranger",
+//     precio: 15530000,
+//     img: "./assets/ranger.jpg",
+//     cantidad: 1,
+//   },
+// ];
+
+const vercarrito = () => {
+  modalcontainer.innerHTML = "";
+  modalcontainer.style.display = "flex";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+  modalHeader.innerHTML = `
     <h1 class"modal-header-title">Carrito.</h1>
     `;
-    modalcontainer.append(modalHeader);
+  modalcontainer.append(modalHeader);
 
-    const modalbutton = document.createElement("h1");
-    modalbutton.innerText ="x";
-    modalbutton.className="modal-header-boton";
+  const modalbutton = document.createElement("h1");
+  modalbutton.innerText = "x";
+  modalbutton.className = "modal-header-boton";
 
-    modalbutton.addEventListener("click", () => {
-        modalcontainer.style.display ="none";
-    });
+  modalbutton.addEventListener("click", () => {
+    modalcontainer.style.display = "none";
+  });
 
-    modalHeader.append(modalbutton);
+  modalHeader.append(modalbutton);
 
-carrito.forEach((item) => {
-        let carritoContent= document.createElement("div")
-        carrito.className="modal-content";
-        carritoContent.innerHTML = `
+  carrito.forEach((item) => {
+    let carritoContent = document.createElement("div");
+    carrito.className = "modal-content";
+    carritoContent.innerHTML = `
         <img src="${item.img}">
         <h3>${item.nombre}</h3>
         <p> ${item.precio} $</p>
         <p> ${item.cantidad}</p>
     `;
-    modalcontainer.append(carritoContent)
+    modalcontainer.append(carritoContent);
 
-    let eliminar =document.createElement("span");
-    eliminar.innerText="X";
-    eliminar.className="Delete";
+    let eliminar = document.createElement("span");
+    eliminar.innerText = "X";
+    eliminar.className = "Delete";
     carritoContent.append(eliminar);
-});
+  });
 
-const total = carrito.reduce((acc,el) => acc + el.precio,0);
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
 
-const totalComprado = document.createElement("div");
-totalComprado.className ="totalcomprado";
-totalComprado.innerHTML = `total a pagar: ${total}$`
-;
-modalcontainer.append(totalComprado);
+  const totalComprado = document.createElement("div");
+  totalComprado.className = "totalcomprado";
+  totalComprado.innerHTML = `total a pagar: ${total}$`;
+  modalcontainer.append(totalComprado);
 };
 
-iconcart.addEventListener("click",vercarrito );
+iconcart.addEventListener("click", vercarrito);
 
-const eliminarProducto= ()=>{
-}
+const eliminarProducto = () => {};
 
-
-const savelocal = ()=>{
-localStorage.setItem("carrito",JSON.stringify (carrito));
+const savelocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
